68a33a0b40c5ef2c72c83d2c7a0acf8d5b0f61ca
Gregory <Gregory@gregory.nl>
2019-03-25 12:06:08 +0100

    added connection.php

--- browserHandler.php
+++ browserHandler.php
@@ -1,20 +1,5 @@
 <?php
-
-$DB_host = "localhost";
-$DB_user = "root";
-$DB_pass = "password";
-$DB_name = "runescape";
-
-
-try
-{
-    $connection = new PDO("mysql:host={$DB_host};dbname={$DB_name}",$DB_user,$DB_pass);
-    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch(PDOException $e)
-{
-    echo $e->getMessage();
-}
+include "connection.php";
 
 if(isset($_POST['search']) && isset($_POST['type'])){
     $search = $_POST['search'];
